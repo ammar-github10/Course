1e9fe910ea7355bd035a86c454c1daede3a87934
ammar-github10 <112925734+ammar-github10@users.noreply.github.com>
2022-10-18 18:46:00 +0530

    Add files via upload

---
+++ learn-springboot/learn-springboot/src/main/java/com/springb/learnspringboot/LearnSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.springb.learnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class LearnSpringbootApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringbootApplication.class, args);
+	}
+
+}
